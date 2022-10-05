d51b00e13f891007d64a33f543c56c18e90f74cb
Sea-Snell <sea_snell@icloud.com>
2022-10-05 00:22:14 -0700

    fix checkpoint loading

--- src/models/base.py
+++ src/models/base.py
@@ -22,9 +22,12 @@ def handle_checkpoint_path(model_checkpoint_path: str, gcloud_project: Optional[
                            gcloud_token: Optional[Any]=None) -> Tuple[str, tempfile.TemporaryDirectory]:
     if model_checkpoint_path.startswith('gcs://'):
         model_checkpoint_path = model_checkpoint_path[len('gcs://'):]
+        model_checkpoint_path = model_checkpoint_path if model_checkpoint_path.endswith('/') else model_checkpoint_path+'/'
+        
         tmp_dir = tempfile.TemporaryDirectory()
+        tmp_dir_name = tmp_dir.name if tmp_dir.name.endswith('/') else tmp_dir.name+'/'
         # download data
-        gcsfs.GCSFileSystem(project=gcloud_project, token=gcloud_token).get(model_checkpoint_path, tmp_dir.name, recursive=True)
+        gcsfs.GCSFileSystem(project=gcloud_project, token=gcloud_token).get(model_checkpoint_path, tmp_dir_name, recursive=True)
         return tmp_dir.name, tmp_dir
     return model_checkpoint_path, None
 
